fe53400cbfcb0ff2862b239d1be1e4406c1c5cc6
Dimitri van Zyl <20113057@vcconnect.co.za>
2021-06-10 09:30:47 +0200

    Add project files.

---
+++ prjIceTask3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace prjIceTask3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lstProducts = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(46, 80);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(87, 15);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Product Name:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(58, 123);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lstProducts
+            // 
+            this.lstProducts.FormattingEnabled = true;
+            this.lstProducts.ItemHeight = 15;
+            this.lstProducts.Location = new System.Drawing.Point(290, 67);
+            this.lstProducts.Name = "lstProducts";
+            this.lstProducts.Size = new System.Drawing.Size(161, 79);
+            this.lstProducts.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(155, 77);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 23);
+            this.txtName.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(155, 123);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear List";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHeading.Location = new System.Drawing.Point(155, 19);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(170, 32);
+            this.lblHeading.TabIndex = 7;
+            this.lblHeading.Text = "PRODUCT LIST";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 169);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lstProducts);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox lstProducts;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblHeading;
+    }
+}
+
---
+++ prjIceTask3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjIceTask3
+{
+    public partial class Form1 : Form
+    {
+        private static string PATH = System.IO.Path.GetFullPath(@"..\..\..\") + "ProductList.txt";
+
+        string[] ProductList;
+
+        string strName;
+        string DELIMITER = ",";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void RunDelimiter ()
+        {
+           
+            strName = txtName.Text;
+            
+            ProductList = strName.Split(DELIMITER);
+            
+            foreach (var Product in ProductList)
+            {
+                lstProducts.Items.Add(Product);
+            }
+            txtName.Clear();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(txtName.Text != "")
+            {
+                RunDelimiter();
+            }
+            else
+            {
+                MessageBox.Show("Please enter in values");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstProducts.Items.Clear();
+        }
+    }
+}
